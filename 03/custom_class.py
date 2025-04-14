146bec7d1e77ba7fef3613fd9980ae71253c95dc
zuck <75354396+Papa-bot@users.noreply.github.com>
2025-04-14 06:50:56 +0300

    fix custom_class
    * fix custom_class

--- 03/custom_class.py
+++ 03/custom_class.py
@@ -1,24 +1,21 @@
+from itertools import zip_longest
+
+
 class CustomList(list):
     def __add__(self, other):
         if isinstance(other, (list, CustomList)):
-            max_len = max(len(self), len(other))
-            new_list = [(self[i] if i < len(self) else 0) +
-                        (other[i] if i < len(other) else 0) for i in range(max_len)]
+            new_list = [x + y for x, y in zip_longest(self, other, fillvalue=0)]
             return CustomList(new_list)
         if isinstance(other, int):
             return CustomList([x + other for x in self])
         raise TypeError("Unsupported type for addition")
 
     def __radd__(self, other):
-        if isinstance(other, (list, CustomList, int)):
-            return self + other
-        raise TypeError("Unsupported type for addition")
+        return self + other
 
     def __sub__(self, other):
         if isinstance(other, (list, CustomList)):
-            max_len = max(len(self), len(other))
-            new_list = [(self[i] if i < len(self) else 0) -
-                        (other[i] if i < len(other) else 0) for i in range(max_len)]
+            new_list = [x - y for x, y in zip_longest(self, other, fillvalue=0)]
             return CustomList(new_list)
         if isinstance(other, int):
             return CustomList([x - other for x in self])
@@ -26,9 +23,7 @@ class CustomList(list):
 
     def __rsub__(self, other):
         if isinstance(other, (list, CustomList)):
-            max_len = max(len(self), len(other))
-            new_list = [(other[i] if i < len(other) else 0) -
-                        (self[i] if i < len(self) else 0) for i in range(max_len)]
+            new_list = [y - x for x, y in zip_longest(self, other, fillvalue=0)]
             return CustomList(new_list)
         if isinstance(other, int):
             return CustomList([other - x for x in self])
@@ -40,9 +35,7 @@ class CustomList(list):
         raise TypeError("Unsupported type for comparison")
 
     def __ne__(self, other):
-        if isinstance(other, CustomList):
-            return sum(self) != sum(other)
-        raise TypeError("Unsupported type for comparison")
+        return not self == other
 
     def __lt__(self, other):
         if isinstance(other, CustomList):
@@ -66,18 +59,3 @@ class CustomList(list):
 
     def __str__(self):
         return f'CustomList({super().__str__()}), sum: {sum(self)}'
-
-
-if __name__ == '__main__':
-    c1 = CustomList([5, 1, 3, 7]) + CustomList([1, 2, 7])
-    print(c1)  # CustomList([6, 3, 10, 7])
-    print(CustomList([10]) + [2, 5])  # CustomList([12, 5])
-    print([2, 5] + CustomList([10]))  # CustomList([12, 5])
-    print(CustomList([2, 5]) + 10)  # CustomList([12, 15])
-    print(10 + CustomList([2, 5]))  # CustomList([12, 15])
-
-    print(CustomList([5, 1, 3, 7]) - CustomList([1, 2, 7]))  # CustomList([4, -1, -4, 7])
-    print(CustomList([10]) - [2, 5])  # CustomList([8, -5])
-    print([2, 5] - CustomList([10]))  # CustomList([-8, 5])
-    print(CustomList([2, 5]) - 10)  # CustomList([-8, -5])
-    print(10 - CustomList([2, 5]))  # CustomList([8, 5])
--- 03/test_custom_class.py
+++ 03/test_custom_class.py
@@ -4,110 +4,182 @@ from custom_class import CustomList
 
 
 class TestCustomList(unittest.TestCase):
-    def setUp(self):
-        self.empty = CustomList()
-        self.single = CustomList([5])
-        self.list1 = CustomList([1, 2, 3])
-        self.list2 = CustomList([4, 5])
-        self.list3 = CustomList([1, 2, 3, 4])
-        self.regular_list1 = [1, 2, 3]
-        self.regular_list2 = [4, 5]
 
     def test_add_two_custom_lists_equal_length(self):
-        result = self.list1 + self.list3
-        self.assertEqual(result, CustomList([2, 4, 6, 4]))
+        list1 = CustomList([1, 2, 3])
+        list2 = CustomList([1, 2, 3, 4])
+        result = list1 + list2
+        self.assertEqual(list(result), [2, 4, 6, 4])
         self.assertIsInstance(result, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
+        self.assertEqual(list(list2), [1, 2, 3, 4])
 
     def test_add_two_custom_lists_different_length(self):
-        result = self.list1 + self.list2
-        self.assertEqual(result, CustomList([5, 7, 3]))
-        self.assertIsInstance(result, CustomList)
+        list1 = CustomList([1, 2, 3])
+        list2 = CustomList([4, 5])
+        result1 = list1 + list2
+        result2 = list2 + list1
+        self.assertEqual(list(result1), [5, 7, 3])
+        self.assertIsInstance(result1, CustomList)
+        self.assertEqual(list(result2), [5, 7, 3])
+        self.assertIsInstance(result2, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
+        self.assertEqual(list(list2), [4, 5])
 
     def test_add_custom_list_and_regular_list(self):
-        result = self.list1 + self.regular_list2
-        self.assertEqual(result, CustomList([5, 7, 3]))
-        self.assertIsInstance(result, CustomList)
+        list1 = CustomList([1, 2, 3])
+        regular_list1 = [4, 5]
+        regular_list2 = [1, 2, 3, 4]
+        result1 = list1 + regular_list1
+        result2 = list1 + regular_list2
+        self.assertEqual(list(result1), [5, 7, 3])
+        self.assertIsInstance(result1, CustomList)
+        self.assertEqual(list(result2), [2, 4, 6, 4])
+        self.assertIsInstance(result2, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_add_custom_list_and_integer(self):
-        result = self.list1 + 5
-        self.assertEqual(result, CustomList([6, 7, 8]))
+        list1 = CustomList([1, 2, 3])
+        result = list1 + 5
+        self.assertEqual(list(result), [6, 7, 8])
         self.assertIsInstance(result, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_add_empty_lists(self):
-        result = self.empty + self.empty
-        self.assertEqual(result, CustomList([]))
+        empty = CustomList()
+        result = empty + empty
+        self.assertEqual(list(result), [])
         self.assertIsInstance(result, CustomList)
+        self.assertEqual(list(empty), [])
 
     def test_add_with_empty_list(self):
-        result = self.list1 + self.empty
-        self.assertEqual(result, CustomList([1, 2, 3]))
+        empty = CustomList()
+        list1 = CustomList([1, 2, 3])
+        result = list1 + empty
+        self.assertEqual(list(result), [1, 2, 3])
         self.assertIsInstance(result, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
+        self.assertEqual(list(empty), [])
 
     def test_add_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            self.list1 + "string"
+            list1 + "string"
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_radd_regular_list_and_custom_list(self):
-        result = self.regular_list1 + self.list2
-        self.assertEqual(result, CustomList([5, 7, 3]))
-        self.assertIsInstance(result, CustomList)
+        list1 = CustomList([4, 5])
+        regular_list1 = [1, 2, 3]
+        regular_list2 = [7]
+        result1 = regular_list1 + list1
+        result2 = regular_list2 + list1
+        self.assertEqual(list(result1), [5, 7, 3])
+        self.assertIsInstance(result1, CustomList)
+        self.assertEqual(list(result2), [11, 5])
+        self.assertIsInstance(result2, CustomList)
+        self.assertEqual(list(list1), [4, 5])
 
     def test_radd_integer_and_custom_list(self):
-        result = 5 + self.list1
-        self.assertEqual(result, CustomList([6, 7, 8]))
+        list1 = CustomList([1, 2, 3])
+        result = 5 + list1
+        self.assertEqual(list(result), [6, 7, 8])
         self.assertIsInstance(result, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_radd_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            "string" + self.list1
+            "string" + list1
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_sub_two_custom_lists_equal_length(self):
-        result = self.list1 - self.list3
-        self.assertEqual(result, CustomList([0, 0, 0, -4]))
-        self.assertIsInstance(result, CustomList)
+        list1 = CustomList([1, 2, 3])
+        list2 = CustomList([2, 4, 6])
+        result1 = list1 - list2
+        result2 = list2 - list1
+        self.assertEqual(list(result1), [-1, -2, -3])
+        self.assertIsInstance(result1, CustomList)
+        self.assertEqual(list(result2), [1, 2, 3])
+        self.assertIsInstance(result2, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
+        self.assertEqual(list(list2), [2, 4, 6])
 
     def test_sub_two_custom_lists_different_length(self):
-        result = self.list1 - self.list2
-        self.assertEqual(result, CustomList([-3, -3, 3]))
-        self.assertIsInstance(result, CustomList)
+        list1 = CustomList([1, 2, 3])
+        list2 = CustomList([1, 2, 3, 4])
+        result1 = list1 - list2
+        result2 = list2 - list1
+        self.assertEqual(list(result1), [0, 0, 0, -4])
+        self.assertIsInstance(result1, CustomList)
+        self.assertEqual(list(result2), [0, 0, 0, 4])
+        self.assertIsInstance(result2, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
+        self.assertEqual(list(list2), [1, 2, 3, 4])
 
     def test_sub_custom_list_and_regular_list(self):
-        result = self.list1 - self.regular_list2
-        self.assertEqual(result, CustomList([-3, -3, 3]))
-        self.assertIsInstance(result, CustomList)
+        list1 = CustomList([1, 2, 3])
+        regular_list1 = [4, 5]
+        regular_list2 = [2, 3, 4, 5]
+        result1 = list1 - regular_list1
+        result2 = list1 - regular_list2
+        self.assertEqual(list(result1), [-3, -3, 3])
+        self.assertIsInstance(result1, CustomList)
+        self.assertEqual(list(result2), [-1, -1, -1, -5])
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_sub_custom_list_and_integer(self):
-        result = self.list1 - 1
-        self.assertEqual(result, CustomList([0, 1, 2]))
+        list1 = CustomList([1, 2, 3])
+        result = list1 - 1
+        self.assertEqual(list(result), [0, 1, 2])
         self.assertIsInstance(result, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_sub_empty_lists(self):
-        result = self.empty - self.empty
-        self.assertEqual(result, CustomList([]))
+        empty = CustomList()
+        result = empty - empty
+        self.assertEqual(list(result), [])
         self.assertIsInstance(result, CustomList)
+        self.assertEqual(list(empty), [])
 
     def test_sub_with_empty_list(self):
-        result = self.list1 - self.empty
-        self.assertEqual(result, CustomList([1, 2, 3]))
+        empty = CustomList()
+        list1 = CustomList([1, 2, 3])
+        result = list1 - empty
+        self.assertEqual(list(result), [1, 2, 3])
         self.assertIsInstance(result, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
+        self.assertEqual(list(empty), [])
 
     def test_sub_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            self.list1 - "string"
+            list1 - "string"
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_rsub_regular_list_and_custom_list(self):
-        result = self.regular_list1 - self.list2
-        self.assertEqual(result, CustomList([-3, -3, 3]))
-        self.assertIsInstance(result, CustomList)
+        list1 = CustomList([4, 5])
+        regular_list1 = [1, 2, 3]
+        regular_list2 = [7]
+        result1 = regular_list1 - list1
+        result2 = regular_list2 - list1
+        self.assertEqual(list(result1), [-3, -3, 3])
+        self.assertIsInstance(result1, CustomList)
+        self.assertEqual(list(result2), [3, -5])
+        self.assertIsInstance(result2, CustomList)
+        self.assertEqual(list(list1), [4, 5])
 
     def test_rsub_integer_and_custom_list(self):
-        result = 5 - self.list1
-        self.assertEqual(result, CustomList([4, 3, 2]))
+        list1 = CustomList([1, 2, 3])
+        result = 5 - list1
+        self.assertEqual(list(result), [4, 3, 2])
         self.assertIsInstance(result, CustomList)
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_rsub_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            "string" - self.list1
+            "string" - list1
+        self.assertEqual(list(list1), [1, 2, 3])
 
     def test_eq_true(self):
         self.assertTrue(CustomList([1, 2, 3]) == CustomList([6]))
@@ -118,28 +190,34 @@ class TestCustomList(unittest.TestCase):
         self.assertFalse(CustomList([1, 2, 3]) == CustomList([1, 1, 1, 4]))
 
     def test_eq_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            self.list1 == [1, 2, 3]
+            list1 == [1, 2, 3]
 
     def test_ne_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            self.list1 != [1, 2, 3]
+            list1 != [1, 2, 3]
 
     def test_lt_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            self.list1 < [1, 2, 3]
+            list1 < [1, 2, 3]
 
     def test_le_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            self.list1 <= [1, 2, 3]
+            list1 <= [1, 2, 3]
 
     def test_gt_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            self.list1 > [1, 2, 3]
+            list1 > [1, 2, 3]
 
     def test_ge_unsupported_type(self):
+        list1 = CustomList([1, 2, 3])
         with self.assertRaises(TypeError):
-            self.list1 >= [1, 2, 3]
+            list1 >= [1, 2, 3]
 
     def test_ne_true(self):
         self.assertTrue(CustomList([1, 2, 3]) != CustomList([7]))
@@ -184,39 +262,31 @@ class TestCustomList(unittest.TestCase):
         self.assertFalse(CustomList([1, 2, 3]) >= CustomList([1, 1, 1, 4]))
 
     def test_str_empty_list(self):
-        self.assertEqual(str(self.empty), "CustomList([]), sum: 0")
+        empty = CustomList()
+        self.assertEqual(str(empty), "CustomList([]), sum: 0")
 
     def test_str_non_empty_list(self):
-        self.assertEqual(str(self.list1), "CustomList([1, 2, 3]), sum: 6")
-        self.assertEqual(str(self.list2), "CustomList([4, 5]), sum: 9")
-
-    def test_original_lists_unchanged_after_add(self):
-        original1 = CustomList([1, 2, 3])
-        original2 = CustomList([4, 5])
-        result = original1 + original2
-        self.assertEqual(original1, CustomList([1, 2, 3]))
-        self.assertEqual(original2, CustomList([4, 5]))
-        self.assertEqual(result, CustomList([5, 7, 3]))
-
-    def test_original_lists_unchanged_after_sub(self):
-        original1 = CustomList([1, 2, 3])
-        original2 = CustomList([4, 5])
-        result = original1 - original2
-        self.assertEqual(original1, CustomList([1, 2, 3]))
-        self.assertEqual(original2, CustomList([4, 5]))
-        self.assertEqual(result, CustomList([-3, -3, 3]))
+        list1 = CustomList([1, 2, 3])
+        list2 = CustomList([4, 5])
+        self.assertEqual(str(list1), "CustomList([1, 2, 3]), sum: 6")
+        self.assertEqual(str(list2), "CustomList([4, 5]), sum: 9")
 
     def test_single_element_list_operations(self):
-        self.assertEqual(self.single + self.single, CustomList([10]))
-        self.assertEqual(self.single - self.single, CustomList([0]))
-        self.assertEqual(self.single + 5, CustomList([10]))
-        self.assertEqual(5 + self.single, CustomList([10]))
-        self.assertEqual(self.single - 2, CustomList([3]))
-        self.assertEqual(10 - self.single, CustomList([5]))
+        single = CustomList([5])
+        self.assertEqual(list(single + single), [10])
+        self.assertEqual(list(single - single), [0])
+        self.assertEqual(list(single + 5), [10])
+        self.assertEqual(list(5 + single), [10])
+        self.assertEqual(list(single - 2), [3])
+        self.assertEqual(list(10 - single), [5])
+        self.assertEqual(list(single), [5])
 
     def test_operations_with_negative_numbers(self):
+        list1 = CustomList([1, 2, 3])
         neg_list = CustomList([-1, -2, -3])
-        self.assertEqual(neg_list + self.list1, CustomList([0, 0, 0]))
-        self.assertEqual(neg_list - self.list1, CustomList([-2, -4, -6]))
-        self.assertEqual(neg_list + (-5), CustomList([-6, -7, -8]))
-        self.assertEqual(neg_list - (-1), CustomList([0, -1, -2]))
+        self.assertEqual(list(neg_list + list1), [0, 0, 0])
+        self.assertEqual(list(neg_list - list1), [-2, -4, -6])
+        self.assertEqual(list(neg_list + (-5)), [-6, -7, -8])
+        self.assertEqual(list(neg_list - (-1)), [0, -1, -2])
+        self.assertEqual(list(list1), [1, 2, 3])
+        self.assertEqual(list(neg_list), [-1, -2, -3])
